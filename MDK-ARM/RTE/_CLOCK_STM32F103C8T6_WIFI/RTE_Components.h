a88acd46057c6b660184a7f495b37ff8b251591d
wenhuafeng <547566993@qq.com>
2022-04-01 23:28:23 +0800

    增加对MDK的编译烧录支持

---
+++ MDK-ARM/RTE/_CLOCK_STM32F103C8T6_WIFI/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'CLOCK_STM32F103C8T6_WIFI' 
+ * Target:  'CLOCK_STM32F103C8T6_WIFI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
