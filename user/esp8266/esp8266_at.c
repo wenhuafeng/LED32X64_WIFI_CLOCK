334efd28faa9e3a4b919249eb43f74a7aa2a741f
wenhuafeng <547566993@qq.com>
2022-04-12 21:01:44 +0800

    优化esp8266返回的时间字符串处理方式，用库函数处理，提升程序运行效率

--- user/esp8266/esp8266_at.c
+++ user/esp8266/esp8266_at.c
@@ -13,29 +13,33 @@
 
 #if (WIFI_MODULE == WIFI_ESP8266)
 
-#define WIFI_CH_PD PBout(5)
+#define WEEK_STR_INDEX          13
+#define MONTH_STR_INDEX         17
+#define DAY_STR_INDEX_HIGH      21
+#define DAY_STR_INDEX_LOW       22
+#define HOUR_STR_INDEX_HIGH     24
+#define HOUR_STR_INDEX_LOW      25
+#define MIN_STR_INDEX_HIGH      27
+#define MIN_STR_INDEX_LOW       28
+#define SEC_STR_INDEX_HIGH      30
+#define SEC_STR_INDEX_LOW       31
+#define YEAR_STR_INDEX_THOUSAND 33
+#define YEAR_STR_INDEX_HUNDRED  34
+#define YEAR_STR_INDEX_TEN      35
+#define YEAR_STR_INDEX          36
 
-#define MON 0x004d6f6e
-#define TUE 0x00547565
-#define WED 0x00576564
-#define THU 0x00546875
-#define FRI 0x00467269
-#define SAT 0x00536174
-#define SUN 0x0053756e
+#define YEAR_MIN  2000
+#define YEAR_MAX  2099
+#define MONTH_MIN 1
+#define MONTH_MAX 12
+#define DAY_MIN   1
+#define DAY_MAX   31
+#define WEEK_MAX  6
+#define HOUR_MAX  23
+#define MIN_MAX   59
+#define SEC_MAX   59
 
-#define JAN 0x004a616e
-#define FEB 0x00466562
-#define MAR 0x004d6172
-#define APR 0x00417072
-#define MAY 0x004d6179
-#define JUN 0x004a756e
-#define JUL 0x004a756c
-#define AUG 0x00417567
-#define SEP 0x00536570
-#define OCT 0x004f6374
-#define NOV 0x004e6f76
-#define DEC 0x00446563
-
+#define WIFI_CH_PD          PBout(5)
 #define WIFI_ON_TIME        (3 * 60) /* 3 min */
 #define WIFI_GET_TIME_TIMES (5U)
 #define WIFI_SET_SNTP_TIMES (5U)
@@ -60,7 +64,8 @@ const char *g_wifiCmdTable[WIFI_INIT_TOTAL_STEP] = {
     "AT+CWMODE=1\r\n",
     "AT+CWJAP_DEF=\"HSG2\",\"13537011631\"\r\n",
     "AT+CIPSNTPCFG=1,8\r\n",
-    "AT+CIPSNTPTIME?\r\n", "AT+GMR\r\n",
+    "AT+CIPSNTPTIME?\r\n",
+    "AT+GMR\r\n",
 };
 
 /* wifi receive data handler */
@@ -120,87 +125,110 @@ static uint8_t AscToHex(uint8_t asc)
     return hex;
 }
 
-static uint8_t GetWeek(uint32_t weekString)
+static uint8_t GetWeek(char *weekString)
 {
     uint8_t i;
     uint8_t tableLen;
-    uint32_t weekTable[] = { SUN, MON, TUE, WED, THU, FRI, SAT };
+    char *weekTable[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
-    tableLen = sizeof(weekTable) / sizeof(uint32_t);
+    tableLen = sizeof(weekTable) / sizeof(weekTable[0]);
     for (i = 0; i < tableLen; i++) {
-        if (weekTable[i] == weekString) {
+        if (strcmp(weekTable[i], weekString) == 0) {
             break;
         }
     }
 
+    TRACE_PRINTF("week:%d\r\n", i);
     return i;
 }
 
-static uint8_t GetMonth(uint32_t monthString)
+static uint8_t GetMonth(char *monthString)
 {
     uint8_t i;
     uint8_t tableLen;
-    uint32_t monthTable[] = { 0, JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
+    char *monthTable[] = { " ", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
-    tableLen = sizeof(monthTable) / sizeof(uint32_t);
+    tableLen = sizeof(monthTable) / sizeof(monthTable[0]);
     for (i = 0; i < tableLen; i++) {
-        if (monthTable[i] == monthString) {
+        if (strcmp(monthTable[i], monthString) == 0) {
             break;
         }
     }
 
+    TRACE_PRINTF("month:%d\r\n", i);
     return i;
 }
 
 static bool ProcessClock(char *cRxBuf)
 {
     uint16_t year;
-    uint8_t month, day, week, hour, minute, second;
-    uint32_t i;
+    uint8_t month;
+    uint8_t day;
+    uint8_t week;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    char str[4] = {0};
     struct TimeType time;
-    bool status = true;
+    int ret;
 
-    i = cRxBuf[13];
-    i = i << 16;
-    i |= cRxBuf[14] << 8 | cRxBuf[15];
-    week = GetWeek(i);
+    ret = snprintf(str, sizeof(str), "%s", &cRxBuf[WEEK_STR_INDEX]);
+    if (ret <= 0) {
+        TRACE_PRINTF("%s snprintf error!\r\n", __LINE__);
+        return false;
+    }
+    week = GetWeek(str);
 
-    i = cRxBuf[17];
-    i = i << 16;
-    i |= cRxBuf[18] << 8 | cRxBuf[19];
-    month = GetMonth(i);
+    ret = snprintf(str, sizeof(str), "%s", &cRxBuf[MONTH_STR_INDEX]);
+    if (ret <= 0) {
+        TRACE_PRINTF("%s snprintf error!\r\n", __LINE__);
+        return false;
+    }
+    month = GetMonth(str);
 
-    day    = AscToHex(cRxBuf[21]) * 10 + AscToHex(cRxBuf[22]);
-    hour   = AscToHex(cRxBuf[24]) * 10 + AscToHex(cRxBuf[25]);
-    minute = AscToHex(cRxBuf[27]) * 10 + AscToHex(cRxBuf[28]);
-    second = AscToHex(cRxBuf[30]) * 10 + AscToHex(cRxBuf[31]);
-    year = AscToHex(cRxBuf[33]) * 1000 + AscToHex(cRxBuf[34]) * 100 + AscToHex(cRxBuf[35]) * 10 + AscToHex(cRxBuf[36]);
+    day    = AscToHex(cRxBuf[DAY_STR_INDEX_HIGH]) * 10 + AscToHex(cRxBuf[DAY_STR_INDEX_LOW]);
+    hour   = AscToHex(cRxBuf[HOUR_STR_INDEX_HIGH]) * 10 + AscToHex(cRxBuf[HOUR_STR_INDEX_LOW]);
+    minute = AscToHex(cRxBuf[MIN_STR_INDEX_HIGH]) * 10 + AscToHex(cRxBuf[MIN_STR_INDEX_LOW]);
+    second = AscToHex(cRxBuf[SEC_STR_INDEX_HIGH]) * 10 + AscToHex(cRxBuf[SEC_STR_INDEX_LOW]);
+    year   = AscToHex(cRxBuf[YEAR_STR_INDEX_THOUSAND]) * 1000 + AscToHex(cRxBuf[YEAR_STR_INDEX_HUNDRED]) * 100 +
+             AscToHex(cRxBuf[YEAR_STR_INDEX_TEN]) * 10 + AscToHex(cRxBuf[YEAR_STR_INDEX]);
 
-    if (year == 1970) {
-        status = false;
-    } else {
-        if ((year < 2000) || (year > 2099) || (month == 0) || (month > 12) || (day == 0) || (day > 31) || (hour > 23) ||
-            (minute > 59) || (second > 59)) {
-            status = false;
-        }
+    if (year < YEAR_MIN || year > YEAR_MAX) {
+        return false;
+    }
+    if (month < MONTH_MIN || month > MONTH_MAX) {
+        return false;
+    }
+    if (day < DAY_MIN || day > DAY_MAX) {
+        return false;
+    }
+    if (week > WEEK_MAX) {
+        return false;
+    }
+    if (hour > HOUR_MAX) {
+        return false;
+    }
+    if (minute > MIN_MAX) {
+        return false;
+    }
+    if (second > SEC_MAX) {
+        return false;
     }
 
-    if (status == true) {
-        time.year  = year;
-        time.month = month;
-        time.day   = day;
-        time.week  = week;
-        time.hour  = hour;
-        time.min   = minute;
-        time.sec   = second;
+    time.year  = year;
+    time.month = month;
+    time.day   = day;
+    time.week  = week;
+    time.hour  = hour;
+    time.min   = minute;
+    time.sec   = second;
 
-        SetTimeData(&time);
-        SetClock(&time);
-        CalculationLunarCalendar(&time);
-        TimeConvertTimestamp(&time);
-    }
+    SetTimeData(&time);
+    SetClock(&time);
+    CalculationLunarCalendar(&time);
+    TimeConvertTimestamp(&time);
 
-    return status;
+    return true;
 }
 
 static void WIFI_Disconnect(char *buf)
@@ -242,6 +270,7 @@ static void WIFI_ReturnTime(char *buf)
 
     ret = ProcessClock(buf);
     if (ret == false) {
+        TRACE_PRINTF("process clock error!\r\n");
         if (g_getTime.getTimeTimes < WIFI_GET_TIME_TIMES) {
             g_getTime.getTimeTimes++;
             g_getTime.getTimeCtr = GET_TIME_DELAY_SEC;
@@ -293,7 +322,7 @@ bool WIFI_Init(void)
 void WIFI_Power(enum PowerFlag flag)
 {
     if (flag == POWER_ON) {
-        WIFI_CH_PD = 1;
+        WIFI_CH_PD                 = 1;
         g_getTime.wifiPowerOffTime = WIFI_ON_TIME;
         TRACE_PRINTF("wifi power on\r\n");
     } else {
